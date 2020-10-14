6369ab6ba1e69606e00d113ac8163812bc9b7975
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-14 01:16:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,5 @@
 class Dog 
-  def initialize(name)
-    @name=name
-  end
-  def name=(name)
-    @name
-  end
-  def name
-    @name
-  end
-  def initialize(breed="Mutt")
-    @breed=breed
-  end
-  def breed=(breed="Mutt")
-    @breed
-  end
-  def breed
-    @breed
-  end
+  def initialize(name, breed="Mutt")
+    
+ 
 end
